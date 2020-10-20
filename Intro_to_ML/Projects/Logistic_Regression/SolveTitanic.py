0219902615773b0440eb4d021215f7dba4bc53d6
Prithivee7 <55012450+Prithivee7@users.noreply.github.com>
2020-10-20 23:08:24 +0530

    Initial commit

---
+++ Intro_to_ML/Projects/Logistic_Regression/SolveTitanic.py
@@ -0,0 +1,188 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 17 12:29:11 2020
+
+@author: Prithivee Ramalingam
+"""
+
+
+import pandas as pd
+import numpy as np
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC, LinearSVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import Perceptron
+from sklearn.linear_model import SGDClassifier
+
+
+direct = 'V:/DataScience_2019501043/Intro_to_ML/Projects/Logistic_Regression'
+test = direct +'/test.csv'
+train = direct +'/train.csv'
+
+test = pd.read_csv(test)
+train = pd.read_csv(train)
+
+train_test_data = [train, test] # combining train and test dataset
+
+for dataset in train_test_data:
+    dataset['Title'] = dataset.Name.str.extract(' ([A-Za-z]+)\.')
+    
+pd.crosstab(train['Title'], train['Sex'])
+
+for dataset in train_test_data:
+    dataset['Title'] = dataset['Title'].replace(['Lady', 'Countess','Capt', 'Col', \
+ 	'Don', 'Dr', 'Major', 'Rev', 'Sir', 'Jonkheer', 'Dona'], 'Other')
+
+    dataset['Title'] = dataset['Title'].replace('Mlle', 'Miss')
+    dataset['Title'] = dataset['Title'].replace('Ms', 'Miss')
+    dataset['Title'] = dataset['Title'].replace('Mme', 'Mrs')
+    
+train[['Title', 'Survived']].groupby(['Title'], as_index=False).mean()
+
+title_mapping = {"Mr": 1, "Miss": 2, "Mrs": 3, "Master": 4, "Other": 5}
+for dataset in train_test_data:
+    dataset['Title'] = dataset['Title'].map(title_mapping)
+    dataset['Title'] = dataset['Title'].fillna(0)
+    
+for dataset in train_test_data:
+    dataset['Sex'] = dataset['Sex'].map( {'female': 1, 'male': 0} ).astype(int)
+    
+for dataset in train_test_data:
+    dataset['Embarked'] = dataset['Embarked'].fillna('S')
+
+for dataset in train_test_data:
+    #print(dataset.Embarked.unique())
+    dataset['Embarked'] = dataset['Embarked'].map( {'S': 0, 'C': 1, 'Q': 2} ).astype(int)
+
+for dataset in train_test_data:
+    age_avg = dataset['Age'].mean()
+    age_std = dataset['Age'].std()
+    age_null_count = dataset['Age'].isnull().sum()
+    
+    age_null_random_list = np.random.randint(age_avg - age_std, age_avg + age_std, size=age_null_count)
+    dataset['Age'][np.isnan(dataset['Age'])] = age_null_random_list
+    dataset['Age'] = dataset['Age'].astype(int)
+    
+train['AgeBand'] = pd.cut(train['Age'], 5)
+
+#print (train[['AgeBand', 'Survived']].groupby(['AgeBand'], as_index=False).mean())
+
+for dataset in train_test_data:
+    dataset.loc[ dataset['Age'] <= 16, 'Age'] = 0
+    dataset.loc[(dataset['Age'] > 16) & (dataset['Age'] <= 32), 'Age'] = 1
+    dataset.loc[(dataset['Age'] > 32) & (dataset['Age'] <= 48), 'Age'] = 2
+    dataset.loc[(dataset['Age'] > 48) & (dataset['Age'] <= 64), 'Age'] = 3
+    dataset.loc[ dataset['Age'] > 64, 'Age'] = 4
+
+for dataset in train_test_data:
+    dataset['Fare'] = dataset['Fare'].fillna(train['Fare'].median())
+    
+train['FareBand'] = pd.qcut(train['Fare'], 4)
+#print (train[['FareBand', 'Survived']].groupby(['FareBand'], as_index=False).mean())
+
+for dataset in train_test_data:
+    dataset.loc[ dataset['Fare'] <= 7.91, 'Fare'] = 0
+    dataset.loc[(dataset['Fare'] > 7.91) & (dataset['Fare'] <= 14.454), 'Fare'] = 1
+    dataset.loc[(dataset['Fare'] > 14.454) & (dataset['Fare'] <= 31), 'Fare']   = 2
+    dataset.loc[ dataset['Fare'] > 31, 'Fare'] = 3
+    dataset['Fare'] = dataset['Fare'].astype(int)
+    
+for dataset in train_test_data:
+    dataset['FamilySize'] = dataset['SibSp'] +  dataset['Parch'] + 1
+
+#print (train[['FamilySize', 'Survived']].groupby(['FamilySize'], as_index=False).mean())
+
+for dataset in train_test_data:
+    dataset['IsAlone'] = 0
+    dataset.loc[dataset['FamilySize'] == 1, 'IsAlone'] = 1
+    
+#print (train[['IsAlone', 'Survived']].groupby(['IsAlone'], as_index=False).mean())
+
+features_drop = ['Name', 'SibSp', 'Parch', 'Ticket', 'Cabin', 'FamilySize']
+train = train.drop(features_drop, axis=1)
+test = test.drop(features_drop, axis=1)
+train = train.drop(['PassengerId', 'AgeBand', 'FareBand'], axis=1)
+
+X_train = train.drop('Survived', axis=1)
+y_train = train['Survived']
+X_test = test.drop("PassengerId", axis=1).copy()
+
+X_train.shape, y_train.shape, X_test.shape
+
+clf = LogisticRegression()
+clf.fit(X_train, y_train)
+y_pred_log_reg = clf.predict(X_test)
+acc_log_reg = round( clf.score(X_train, y_train) * 100, 2)
+print (str(acc_log_reg) + ' percent')
+
+clf = SVC()
+clf.fit(X_train, y_train)
+y_pred_svc = clf.predict(X_test)
+acc_svc = round(clf.score(X_train, y_train) * 100, 2)
+print (acc_svc)
+
+clf = LinearSVC()
+clf.fit(X_train, y_train)
+y_pred_linear_svc = clf.predict(X_test)
+acc_linear_svc = round(clf.score(X_train, y_train) * 100, 2)
+print (acc_linear_svc)
+
+clf = KNeighborsClassifier(n_neighbors = 3)
+clf.fit(X_train, y_train)
+y_pred_knn = clf.predict(X_test)
+acc_knn = round(clf.score(X_train, y_train) * 100, 2)
+print (acc_knn)
+
+clf = DecisionTreeClassifier()
+clf.fit(X_train, y_train)
+y_pred_decision_tree = clf.predict(X_test)
+acc_decision_tree = round(clf.score(X_train, y_train) * 100, 2)
+print (acc_decision_tree)
+
+clf = RandomForestClassifier(n_estimators=100)
+clf.fit(X_train, y_train)
+y_pred_random_forest = clf.predict(X_test)
+acc_random_forest = round(clf.score(X_train, y_train) * 100, 2)
+print (acc_random_forest)
+
+clf = GaussianNB()
+clf.fit(X_train, y_train)
+y_pred_gnb = clf.predict(X_test)
+acc_gnb = round(clf.score(X_train, y_train) * 100, 2)
+print (acc_gnb)
+
+clf = Perceptron(max_iter=5, tol=None)
+clf.fit(X_train, y_train)
+y_pred_perceptron = clf.predict(X_test)
+acc_perceptron = round(clf.score(X_train, y_train) * 100, 2)
+print (acc_perceptron)
+
+clf = SGDClassifier(max_iter=5, tol=None)
+clf.fit(X_train, y_train)
+y_pred_sgd = clf.predict(X_test)
+acc_sgd = round(clf.score(X_train, y_train) * 100, 2)
+print (acc_sgd)
+
+
+models = pd.DataFrame({
+    'Model': ['Logistic Regression', 'Support Vector Machines', 'Linear SVC', 
+              'KNN', 'Decision Tree', 'Random Forest', 'Naive Bayes', 
+              'Perceptron', 'Stochastic Gradient Decent'],
+    
+    'Score': [acc_log_reg, acc_svc, acc_linear_svc, 
+              acc_knn,  acc_decision_tree, acc_random_forest, acc_gnb, 
+              acc_perceptron, acc_sgd]
+    })
+
+models.sort_values(by='Score', ascending=False)
+
+submission = pd.DataFrame({
+        "PassengerId": test["PassengerId"],
+        "Survived": y_pred_random_forest
+    })
+
+submission.to_csv('submission.csv', index=False)
