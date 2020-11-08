1eb758f4e44a7f7a0b2c7c8474bde0a6120b23ed
Prithivee7 <55012450+Prithivee7@users.noreply.github.com>
2020-11-08 08:13:25 +0530

    Initial commit

--- Data_Mining/Final_exam/apriori.R
+++
@@ -1,6 +0,0 @@
-install.packages("arules")
-data <-read.csv("V:/DataScience_2019501043/Data_Mining/Final_exam/aprori.csv")
-data
-rules <- apriori(data,  
-                 parameter = list(supp = 0.01, conf = 0.2))
-inspect(rules[1:10]) 
